cfc517ce1f109a51d144a141e626ae4114afdd37
Alan Cardero <alan.cardero@gmail.com>
2018-08-06 14:16:51 -0400

    adding extra directive examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { MyEventBindingComponent } from './my-event-binding/my-event-binding.com
 import { MyMixedBindingComponent } from './my-mixed-binding/my-mixed-binding.component';
 import { MyTwoWayBindingComponent } from './my-two-way-binding/my-two-way-binding.component';
 import { MyEventBinding2Component } from './my-event-binding2/my-event-binding2.component';
+import { NgIfExampleComponent } from './ng-if-example/ng-if-example.component';
+import { NgForExampleComponent } from './ng-for-example/ng-for-example.component';
+import { NgSwitchExampleComponent } from './ng-switch-example/ng-switch-example.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,10 @@ import { MyEventBinding2Component } from './my-event-binding2/my-event-binding2.
     MyEventBindingComponent,
     MyMixedBindingComponent,
     MyTwoWayBindingComponent,
-    MyEventBinding2Component
+    MyEventBinding2Component,
+    NgIfExampleComponent,
+    NgForExampleComponent,
+    NgSwitchExampleComponent
   ],
   imports: [
     BrowserModule,
